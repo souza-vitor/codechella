c9ca2ec6ec94a97598e705665d639f8cbaaf4480
Vitor Souza <vitorhugovhps@outlook.com>
2023-03-11 09:56:44 -0300

    formulario da pagina ingresso criado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 const accordionItem = document.querySelectorAll('.accordion__item--question');
-console.log("hi");
-
 accordionItem.forEach(item => {
     item.addEventListener('click', event => {
         item.classList.toggle('active');
